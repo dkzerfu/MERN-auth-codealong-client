042071c789cae38d6112e3b54e7cb2e15c5fc02c
weston-bailey <wdbailey87@gmail.com>
2021-03-18 16:12:35 -0700

    populate nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,29 @@
-export default function Navbar() {
+import { Link } from 'react-router-dom'
+
+export default function Navbar(props) {
   return (
-    <div>
-      hello from navbar
-    </div>
+    <nav>
+      <Link to='/'>
+        <h5>user app 👋</h5>
+      </Link>
+
+      {/* if the user is logged in */}
+      <Link to='/'>
+        <span onClick={props.handleLogout}>log out</span>
+      </Link>
+
+      <Link to="/profile">
+        profile
+      </Link>
+
+      {/* if the use is logged out */}
+      <Link to="/register">
+        register
+      </Link>
+
+      <Link to="/login">
+        login
+      </Link>
+    </nav>
   )
 }
