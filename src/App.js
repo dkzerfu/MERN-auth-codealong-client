f6330b08b3fd065ea9f97a4e9ece445a9e6ac786
weston-bailey <wdbailey87@gmail.com>
2021-03-18 16:04:19 -0700

    make app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import Register from './components/Register'
+import Welcome from './components/Welcome'
 import './App.css';
 
 function App() {
-  console.log(process.env.REACT_APP_SERVER_URL)
+  // user data will be in state when the user is logged in 
+  const [currentUser, setCurrentUser] = useState(null)
+
+  // useEffect if the user navigates away from the site and comes back
+
+  // deletes the jwt from local storage when the user wants to log out
+  const handleLogout = () => {
+    console.log('log user out')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <Navbar currentUser={ currentUser } handleLogout={ handleLogout } />
       </header>
-    </div>
+
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={ Welcome } />
+
+          <Route 
+            path='/register'
+            render={ (props) => <Register {...props} currentUser={ currentUser } setCurrentUser={ setCurrentUser} /> } 
+          />
+
+          <Route 
+            path='/login'
+            render={ (props) => <Login {...props} currentUser={ currentUser } setCurrentUser={ setCurrentUser} /> } 
+          />
+
+          <Route 
+            path='/profile'
+            render={ (props) => <Profile {...props} currentUser={ currentUser } setCurrentUser={ setCurrentUser} /> } 
+          />
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
